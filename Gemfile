684ba6e47807b0ebdb3528d56726a9a947f46ae4
NevenRakonic <neven.rakonic@gmail.com>
2016-12-28 15:51:43 +0100

    Test contact creation completely

--- Gemfile
+++ Gemfile
@@ -54,6 +54,11 @@ group :development, :test do
   gem 'factory_girl_rails'
 end
 
+group :test do
+  gem 'poltergeist'
+  gem 'database_cleaner'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- spec/features/contact_creation_spec.rb
+++ spec/features/contact_creation_spec.rb
@@ -16,7 +16,7 @@ describe "Contact creation" do
         expect(page).to have_link 'Add Attribute'
       end
 
-      context "User submits the form without added attributes correctly" do
+      context "and submits the form without added attributes correctly" do
         before do
           fill_in "contact_first_name", with: "Jack"
           fill_in "contact_last_name", with: "Example"
@@ -24,39 +24,88 @@ describe "Contact creation" do
           click_button 'Create Contact'
         end
 
-        specify 'User is shown the flash message' do
+        specify 'a flash message is shown' do
           expect(page).to have_content 'Contact succesfully created'
         end
 
-        specify 'User is back on the updated index page' do
+        specify 'user is back on the updated index page' do
           expect(page).to have_content "Jack"
           expect(page).to have_content "Example"
           expect(page).to have_content "Baker Street 25"
         end
       end
 
-      context "User adds an attribute", js: :true do
+      context "adds an attribute", js: :true do
         before do
-          click_button 'Add Attribute'
+          click_link 'Add Attribute'
         end
 
-        specify 'User can see attribute form fields' do
-          expect(page).to have_field(/contact_contact_attributes_attributes_*_name/)
-          expect(page).to have_field(/contact_contact_attributes_attributes_*_content/)
+        specify 'he can see attribute form fields' do
+          expect(page).to have_field("Name")
+          expect(page).to have_field("Content")
+        end
+
+        context "form with contact attributes is correctly submitted" do
+          before do
+            fill_in "contact_first_name", with: "Jack"
+            fill_in "contact_last_name", with: "Example"
+            fill_in "contact_address", with: "Baker Street 25"
+            fill_in "Name", with: "E-mail"
+            fill_in "Content", with: "contact@example.com"
+            click_button 'Create Contact'
+          end
+
+          specify 'flash message is shown' do
+            expect(page).to have_content 'Contact succesfully created'
+          end
+
+          specify 'user is back on the updated index page' do
+            expect(page).to have_content "Jack"
+            expect(page).to have_content "Example"
+            expect(page).to have_content "Baker Street 25"
+          end
+        end
+
+        context "form with contact attributes is incorrectly submitted" do
+          before do
+            fill_in "contact_first_name", with: "Jack"
+            fill_in "contact_last_name", with: "Example"
+            fill_in "contact_address", with: "Baker Street 25"
+            fill_in "Name", with: ""
+            fill_in "Content", with: "contact@example.com"
+            click_button 'Create Contact'
+          end
+
+          specify 'user is shown the validation errors' do
+            expect(page).to have_content "can't be blank"
+          end
+
+          specify 'user can still see the form' do
+            expect(page).to have_field("contact_first_name")
+            expect(page).to have_field("contact_last_name")
+            expect(page).to have_field("contact_address")
+            expect(page).to have_field("contact_mobile_phone")
+            expect(page).to have_field("contact_fixed_phone")
+            expect(page).to have_field("contact_image")
+            expect(page).to have_field("Name")
+            expect(page).to have_field("Content")
+            expect(page).to have_link 'Remove Attribute'
+            expect(page).to have_link 'Add Attribute'
+          end
         end
       end
 
-      context "User submits the form without added attributes incorrectly" do
+      context "form without added attributes is submitted incorrectly" do
         before do
           fill_in "contact_address", with: "Baker Street 25"
           click_button 'Create Contact'
         end
 
-        specify 'User is shown the validation errors' do
+        specify 'user is shown the validation errors' do
           expect(page).to have_content "can't be blank"
         end
 
-        specify 'User can still see the form' do
+        specify 'user can still see the form' do
           expect(page).to have_field("contact_first_name")
           expect(page).to have_field("contact_last_name")
           expect(page).to have_field("contact_address")
